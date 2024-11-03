be239fcf090b5d94d3cf36b19f26d9d761e1c6ba
Kuzminskiy Andrey <s02200417@gse.cs.msu.ru>
2024-11-04 01:32:28 +0300

    07_Environmental

---
+++ 07_Environmental/src/rhasher.c
@@ -0,0 +1,87 @@
+#include <stdio.h>
+#include <rhash.h>
+#include <stdlib.h>
+#include <string.h>
+#include "config.h"
+#include <ctype.h>
+
+#ifdef USE_READLINE
+#include <readline/readline.h>
+#include <readline/history.h>
+#endif
+
+int hash_file(const char* filename, int hash_type, int output_type, unsigned char* digest, char* output) {
+    rhash_file(hash_type, filename, digest);
+    rhash_print_bytes(output, digest, rhash_get_digest_size(hash_type), output_type);
+    printf("%s\n", output);
+
+    return 0;
+}
+
+int hash_string(const char* string, int hash_type, int output_type, unsigned char* digest, char* output) {
+    rhash_msg(hash_type, string, strlen(string), digest);
+    rhash_print_bytes(output, digest, rhash_get_digest_size(hash_type), output_type);
+    printf("%s\n", output);
+
+    return 0;
+}
+
+
+int main(int argc, char** argv) {
+    char delim = ' ';
+    unsigned char digest[64];
+    char output[130];
+    
+    int hash_type, output_type;
+
+    rhash_library_init();\
+    
+   
+#ifndef ENABLE_READLINE
+    char* line;
+    size_t lenght = 0;
+    ssize_t count;
+    while((count = getline(&line, &lenght, stdin)) != -1) {
+#else
+    char* line = NULL;
+    printf("READLINE ENABLED!\n");
+    while((line = readline("> ")) != NULL) {
+#endif
+        char* cry_type_str = strtok(line, &delim);
+        char* input = strtok(NULL, &delim);
+
+        if (cry_type_str == NULL || input == NULL) {
+            fprintf(stderr, "Введите алгоритм шифрования (Доступны sha1, md5, tth) и через пробел имя файла или строку через \"\"!\n");
+            continue;
+        }
+
+        if (strcasecmp(cry_type_str, "sha1") == 0) {
+            hash_type = RHASH_SHA1;
+        } else if (strcasecmp(cry_type_str, "md5") == 0) {
+            hash_type = RHASH_MD5;
+        } else if (strcasecmp(cry_type_str, "tth") == 0) {
+            hash_type = RHASH_TTH;
+        } else {
+            fprintf(stderr, "Доступные алгоритмы шифрования - sha1, md5, tth!\n");
+            continue;
+        }
+
+        if(isupper(cry_type_str[0])) {
+            output_type = RHPR_HEX;
+        } else {
+            output_type = RHPR_BASE64;
+        }
+
+        output_type |= RHPR_UPPERCASE;
+
+        if (input[0] == '"') {
+            hash_string(input, hash_type, output_type, digest, output);
+        } else {
+            hash_file(input, hash_type, output_type, digest, output);
+        }
+
+        free(line);
+    }
+
+    return 0;
+}
